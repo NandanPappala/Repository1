a00ccc67bd7cee12d69c8b49a97f6ef82c4d099a
Nandan <nandan.pappala@gmail.com>
2026-08-07 16:29:50 -0400

    Add document content processing using string  value and test sample output for text chunks

--- processdocuments.py
+++ processdocuments.py
@@ -9,3 +9,16 @@ splitter = RecursiveCharacterTextSplitter(
     is_separator_regex=False,
 )
 
+
+
+document_content = """You will also learn about the different levels of Git configuration, specifically global and local settings, and how to list them. Finally, we will briefly touch upon how to handle cases where the user name is not yet configured. This lab will equip you with the knowledge to ensure your Git setup correctly identifies your contributions.
+Use git config user.name to Check
+In this step, we will learn how to check your Git configuration, specifically your user name. Git uses this information to identify who made each commit."""
+
+chunks = splitter.split_text(document_content)
+
+# 3. Output results
+print(f"Created {len(chunks)} chunks.")
+for i, chunk in enumerate(chunks[:3]):
+    print(f"--- Chunk {i+1} ---")
+    print(chunk[:200]) # Print first 150 characters
